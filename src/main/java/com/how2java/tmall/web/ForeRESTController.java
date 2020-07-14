97a66babe8682c5b9a70b61c012b4c9d370de97c
nfslover <chenxiaowei1995@gmail.com>
2020-07-14 15:59:33 +0800

    前台页面home

--- src/main/java/com/how2java/tmall/web/ForeRESTController.java
+++ src/main/java/com/how2java/tmall/web/ForeRESTController.java
@@ -2,11 +2,15 @@ package com.how2java.tmall.web;
 
 
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.User;
 import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.service.ProductService;
+import com.how2java.tmall.service.UserService;
+import com.how2java.tmall.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -16,6 +20,8 @@ public class ForeRESTController {
     CategoryService categoryService;
     @Autowired
     ProductService productService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/forehome")
     public Object home(){
@@ -24,5 +30,25 @@ public class ForeRESTController {
         productService.fillByRow(cs);
         categoryService.removeCategoryFromProduct(cs);
         return cs;
+    }
+
+    @ResponseBody
+    @PostMapping("/foreregister")
+    public Object register(@RequestBody User user){
+        String name = user.getName();
+        String password = user.getPassword();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);
+
+        if(exist){
+            String message = "用户名已被占用，不可使用";
+            return Result.fail(message);
+        }
+        user.setPassword(password);
+
+        userService.add(user);
+        return Result.success();
+
     }
 }
